5d6371afc82d56dab207d6043c8eaa2dcfef3609
evolentDelight <evolentDelight@outlook.com>
2024-06-27 17:18:49 -0700

    Constructor

---
+++ main/Array/Constructor.js
@@ -0,0 +1,17 @@
+new Array()
+new Array(element1)
+new Array(element1, element2)
+new Array(element1, element2, /* …, */ elementN)
+new Array(arrayLength)
+
+Array()
+Array(element1)
+Array(element1, element2)
+Array(element1, element2, /* …, */ elementN)
+Array(arrayLength)
+
+//Array can be created with or without "new"
+// arrayLength must be in integer to define the Array length via constructor
+
+//Literal Notation to create an Array
+const array = ["one", "two"];
