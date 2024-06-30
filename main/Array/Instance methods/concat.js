c04739fb4f9726ee2be9a23843f3b7baec227ab7
evolentDelight <evolentDelight@outlook.com>
2024-06-29 22:21:57 -0700

    Array.prototype.concat()

---
+++ main/Array/Instance methods/concat.js
@@ -0,0 +1,11 @@
+const newArray = Array1.concat(Array2);
+// Method used to merge two or more arrays.
+//  This method does not change the existing arrays, but instead returns a new array
+// Parameters can be arrays and/or values
+
+// Syntax
+concat()
+concat(value1)
+concat(value1, value2)
+concat(value1, value2, /* …, */ valueN)
+
