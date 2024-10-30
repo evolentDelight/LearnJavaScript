b2e632e3252f2d06d87caf40904b26d89cb00087
evolentDelight <evolentDelight@outlook.com>
2024-10-29 20:15:15 -0700

    5.3 Everyting - Finished

--- EloquentJavaScript/Code Exercises/Chapter 5/Everything.js
+++ EloquentJavaScript/Code Exercises/Chapter 5/Everything.js
@@ -29,3 +29,27 @@ console.log(everySome([2, 4, 16], n => n < 10));
 // → false
 console.log(everySome([], n => n < 10));
 // → true
+
+//Textbook Solutions
+
+function every(array, predicate) {
+  for (let element of array) {
+    if (!predicate(element)) return false;
+  }
+  return true;
+}
+
+function every2(array, predicate) {
+  return !array.some(element => !predicate(element));
+}
+
+//Differences
+  //every with Loop are not different
+  //every with Some method
+    //De Morgan's Law... First time seeing it...
+    //a && b === !(!a || !b)
+    //Because .some method is a matter of || operator between the elements after predicate/test
+      //by applying that law with adjustments, you get a && b
+      //Mind Blown... but took too long trying to figure that out.
+        //Learning efficiency issue...
+          
