fce7cdeffcc5bfe27306646eb7bbd147e8d5d4b9
evolentDelight <evolentDelight@outlook.com>
2025-01-13 19:15:58 -0800

    6.2 Groups complete

--- EloquentJavaScript/Code Exercises/Chapter 6/Groups.js
+++ EloquentJavaScript/Code Exercises/Chapter 6/Groups.js
@@ -44,3 +44,36 @@ group.add(10);
 group.delete(10);
 console.log(group.has(10));
 // → false
+
+//Textbook Solution
+class Group {
+  #members = [];
+
+  add(value) {
+    if (!this.has(value)) {
+      this.#members.push(value);
+    }
+  }
+
+  delete(value) {
+    this.#members = this.#members.filter(v => v !== value);
+  }
+
+  has(value) {
+    return this.#members.includes(value);
+  }
+
+  static from(collection) {
+    let group = new Group;
+    for (let value of collection) {
+      group.add(value);
+    }
+    return group;
+  }
+}
+
+//Differences
+//Didn't follow directions and added wrong constructor
+  //Creating a new group and using its add method would have sufficed
+    //As shown in the textbook solution
+//Delete uses filter which is one line code VS my two line code using splice
