cf28de871d85cf40e48786f52952b6f403bd540f
evolentDelight <evolentDelight@outlook.com>
2025-03-12 11:00:58 -0700

    9.1 Regexp Golf Finished

--- EloquentJavaScript/Code Exercises/Chapter 9/Regexp_Golf.js
+++ EloquentJavaScript/Code Exercises/Chapter 9/Regexp_Golf.js
@@ -1,41 +1,51 @@
 // Fill in the regular expressions
 
-verify(/.../,
+//Match: car and cat
+verify(/ca[rt]/,
   ["my car", "bad cats"],
   ["camper", "high art"]);
 
-verify(/.../,
+//Match: pop and prop
+verify(/pr?op/,
   ["pop culture", "mad props"],
   ["plop", "prrrop"]);
 
-verify(/.../,
+//Match: ferret, ferry, and ferrari
+verify(/ferr[et|y|ari]/,
   ["ferret", "ferry", "ferrari"],
   ["ferrum", "transfer A"]);
 
-verify(/.../,
+//Match: any word ending in ious
+verify(/.ious\b/,
   ["how delicious", "spacious room"],
   ["ruinous", "consciousness"]);
 
-verify(/.../,
+//Match: Whitespace character followed by
+// a period, comma, colon, or semicolon
+verify(/\s[.,:;]/,
   ["bad punctuation ."],
   ["escape the period"]);
 
-verify(/.../,
+//Match: Word longer than six letters
+verify(/\w{7,}/,
   ["Siebentausenddreihundertzweiundzwanzig"],
   ["no", "three small words"]);
 
-verify(/.../,
+//Match: word without the letter e or E
+verify(/(^|\P{L})[^\P{L}e]+($|\P{L})/ui,
   ["red platypus", "wobbling nest"],
   ["earth bed", "bedrøvet abe", "BEET"]);
 
 
 function verify(regexp, yes, no) {
 // Ignore unfinished exercises
-if (regexp.source == "...") return;
-for (let str of yes) if (!regexp.test(str)) {
-console.log(`Failure to match '${str}'`);
-}
-for (let str of no) if (regexp.test(str)) {
-console.log(`Unexpected match for '${str}'`);
-}
+  if (regexp.source == "...") return;
+  console.log("On ", regexp, ": ")
+  for (let str of yes) if (!regexp.test(str)) {
+    console.log(`Failure to match '${str}'`);
+  }
+  for (let str of no) if (regexp.test(str)) {
+    console.log(`Unexpected match for '${str}'`);
+  }
+  console.log("If no failure or unexpected, Success\n")
 }
